8c13f0928fc27c2ec7c94a5066e7979e8eacfa77
Christopher de Oliveira <christopherforwork@gmail.com>
2025-11-17 18:38:50 -0400

    feat: integrate Tailwind CSS and update App component
    - Added Tailwind CSS and its Vite plugin to dependencies.
    - Updated App component to display a "Hello World!" message styled with Tailwind.
    - Imported Tailwind CSS in the global styles.
    - Configured Vite to use Tailwind CSS plugin.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    </>
+  );
 }
 
 export default App;
