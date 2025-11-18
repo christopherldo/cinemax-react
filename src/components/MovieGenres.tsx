65f3df287ffc8c36f2dde520fec4ca1e9cf9faf4
Christopher de Oliveira <christopherforwork@gmail.com>
2025-11-18 18:45:03 -0400

    fix: add conditional rendering for MovieGenre component based on genreIds length

--- src/components/MovieGenres.tsx
+++ src/components/MovieGenres.tsx
@@ -9,7 +9,7 @@ export const MovieGenres = ({ genreIds }: MovieGenresProps) => {
   return (
     <div className="flex flex-wrap items-center gap-1">
       <Film size={16} color="#92929D" />
-      <MovieGenre genreId={genreIds[0]} />
+      {genreIds.length > 0 && <MovieGenre genreId={genreIds[0]} />}
       <div className="h-full w-px bg-[#696974]" />
       <span className="font-medium text-white text-[12px]">Movie</span>
     </div>
