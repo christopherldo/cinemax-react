5518680b058ad825a28bd92184c666fed0ba8acf
Christopher de Oliveira <christopherforwork@gmail.com>
2025-11-19 21:21:21 -0400

    feat: refactor NavBar to use NavBarLink component for improved code organization

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,13 @@
-import { Download, Home, Search, User } from "lucide-react";
+import { NavBarLink } from "./NavBarLink";
 
 export const NavBar = () => {
   return (
     <nav>
       <ul className="h-18 bg-dark fixed bottom-0 w-full flex gap-4 justify-center items-center">
-        <li className="bg-soft rounded-2xl">
-          <a className="w-23 h-10 flex justify-center items-center gap-1">
-            <Home color="#12CDD9" size={24} />
-            <span className="text-blue-accent font-medium block">Home</span>
-          </a>
-        </li>
-        <li>
-          <a className="w-12 h-10 flex justify-center items-center">
-            <Search color="#92929D" size={24} />
-          </a>
-        </li>
-        <li>
-          <a className="w-12 h-10 flex justify-center items-center">
-            <Download color="#92929D" size={24} />
-          </a>
-        </li>
-        <li>
-          <a className="w-12 h-10 flex justify-center items-center">
-            <User color="#92929D" size={24} />
-          </a>
-        </li>
+        <NavBarLink to="/" icon="home" label="Home" />
+        <NavBarLink to="/search" icon="search" label="Search" />
+        <NavBarLink to="/download" icon="download" label="Download" />
+        <NavBarLink to="/profile" icon="user" label="Profile" />
       </ul>
     </nav>
   );
---
+++ src/components/NavBarLink.tsx
@@ -0,0 +1,35 @@
+import { DynamicIcon, dynamicIconImports } from "lucide-react/dynamic";
+import { NavLink } from "react-router-dom";
+
+interface NavBarLinkProps {
+  to: string;
+  icon: keyof typeof dynamicIconImports;
+  label: string;
+}
+
+export const NavBarLink = ({ to, icon, label }: NavBarLinkProps) => {
+  return (
+    <li>
+      <NavLink to={to}>
+        {({ isActive }) => (
+          <div
+            className={`w-auto px-4 h-10 flex justify-center items-center gap-1 rounded-2xl ${
+              isActive && "bg-soft"
+            }`}
+          >
+            <DynamicIcon
+              name={icon}
+              color={isActive ? "#12CDD9" : "#92929D"}
+              size={24}
+            />
+            {isActive && (
+              <span className="text-blue-accent font-medium block">
+                {label}
+              </span>
+            )}
+          </div>
+        )}
+      </NavLink>
+    </li>
+  );
+};
